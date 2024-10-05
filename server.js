c7d8f85bb2eaf411edb6ae1ad1f3eb3693e2bb34
matmaxgraphics <gbadamosimateen@gmail.com>
2024-10-05 12:38:19 +0100

    enabled cors

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
+const cors = require('cors');
+app.use(cors());
+
 const app = express();
 const port = process.env.PORT || 3001;
 
