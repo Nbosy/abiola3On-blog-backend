0154a548d7f1567882f95b86bb590bacd908691e
matmaxgraphics <gbadamosimateen@gmail.com>
2024-10-06 12:54:13 +0100

    created POST function for creating users

--- server.js
+++ server.js
@@ -1,21 +1,24 @@
-const express = require('express');
-const { createClient } = require('@supabase/supabase-js');
-require('dotenv').config();
+const express = require("express");
+const { createClient } = require("@supabase/supabase-js");
+require("dotenv").config();
 
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 3001;
 app.use(cors());
 
 // Initialize Supabase client with service role key
-const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
 // Example route to get all users (Admin API)
-app.get('/users', async (req, res) => {
+app.get("/users", async (req, res) => {
   try {
     const { data: users, error } = await supabase.auth.admin.listUsers();
     if (error) throw error;
@@ -26,21 +29,44 @@ app.get('/users', async (req, res) => {
   }
 });
 
-app.delete('/delete-user/:id', async (req, res) => {
-  const userId = req.params.id;
-
+app.post("/create-user", async (req, res) => {
   try {
-      const { data, error } = await supabase.auth.admin.deleteUser(userId);
-      
-      if (error) throw error;
-      
-      res.status(200).json({ message: 'User deleted successfully', data });
+    const { email, password, full_name, role } = req.body;
+
+    // if (!email || !password || !full_name || !role) {
+    //   return res.status(400).json({ error: "All fields are required" });
+    // }
+    const { data, error } = await supabase.auth.admin.createUser({
+      email: email,
+      password: password,
+      user_metadata: {
+        full_name: full_name,
+        role: role,
+      },
+    });
+
+    if (error) throw error;
+    res.status(200).json({ message: "user created successfully", data });
   } catch (error) {
-      console.error('Error deleting user:', error.message);
-      res.status(500).json({ error: 'Failed to delete user' });
+    console.error("Error creating user:", error.message);
+    res.status(500).json({ error: error.message });
   }
 });
 
+app.delete("/delete-user/:id", async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const { data, error } = await supabase.auth.admin.deleteUser(userId);
+
+    if (error) throw error;
+
+    res.status(200).json({ message: "User deleted successfully", data });
+  } catch (error) {
+    console.error("Error deleting user:", error.message);
+    res.status(500).json({ error: "Failed to delete user" });
+  }
+});
 
 // Start the server
 app.listen(port, () => {
